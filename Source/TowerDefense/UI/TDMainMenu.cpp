8f4b6c29649d245492f845958cf9e136081bc00f
Guille <sokerwild96@gmail.com>
2023-02-09 18:44:43 +0100

    MainMenu Base y parte de Utilities de UI creadas

---
+++ Source/TowerDefense/UI/TDMainMenu.cpp
@@ -0,0 +1,22 @@
+#include "UI/TDMainMenu.h"
+
+bool UTDMainMenu::Initialize()
+{
+    Super::Initialize();
+
+    return true;
+}
+
+void UTDMainMenu::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+
+}
+
+void UTDMainMenu::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+
+}
