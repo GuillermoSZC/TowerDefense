6c4a9f512a33268ca2b80ab430bc055b5ec789ad
Guille <sokerwild96@gmail.com>
2023-03-31 12:18:59 +0200

    Refactorizacion de la data table de generacion de enemigos

--- Source/TowerDefense/GameLogic/TDEnemiesDataTable.h
+++ Source/TowerDefense/GameLogic/TDEnemiesDataTable.h
@@ -18,71 +18,65 @@ public:
     ~FTDEnemiesDataTable();
 
 public:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region ENEMY_MESH
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Mesh")
         TSoftObjectPtr<USkeletalMesh> enemyMesh;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Mesh")
         FVector MeshPosition = FVector::ZeroVector;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Enemy Mesh")
         FVector MeshScale = FVector(1.f);
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region HUD
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
         FVector2D HealthBarSize = FVector2D(1.2f, 0.4f);
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HUD")
         FVector HealthBarPosition = FVector(0.f, -40.f, 160.f);
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region CAPSULE_VALUES
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Capsule Values")
         float capsuleHeight = 130.f;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Capsule Values")
         float capsuleRadius = 55.f;
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region ANIMATION
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
         TSubclassOf<UAnimInstance> animationBlueprint;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
         TSoftObjectPtr<class UAnimMontage> animationMontaje;
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region ROUND_VALUES
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Round Values")
         int32 weight;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Round Values")
         int32 firstPossibleApperance;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Round Values")
         int32 limitEnemiesPerRound;
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region GAS_VALUES
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay Ability System Values")
         UTDEnemyStats* EnemyStatsDataAsset;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay Ability System Values")
         float movementVariation;
 
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Gameplay Ability System Values")
         TArray<TSubclassOf<class UGameplayAbility>> abiliyList;
+#pragma endregion
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+#pragma region AI
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "AI")
         TSoftObjectPtr<class UBehaviorTree> behaviorTree;
-
-
-
-
-protected:
-
-
-private:
-
-
-public:
-
-
-protected:
-
-
-private:
-
-
+#pragma endregion
 };
