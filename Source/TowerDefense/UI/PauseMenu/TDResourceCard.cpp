efe55959f83428f9f607aa9fdc84e721ebc04fd8
Guille <sokerwild96@gmail.com>
2023-05-29 20:46:07 +0200

    ResourceCard estructura creada

--- Source/TowerDefense/UI/PauseMenu/TDResourceCard.cpp
+++ Source/TowerDefense/UI/PauseMenu/TDResourceCard.cpp
@@ -1,2 +1,32 @@
 #include "UI/PauseMenu/TDResourceCard.h"
+#include <Engine/Texture2D.h>
+#include <UMG/Public/Components/Image.h>
 
+bool UTDResourceCard::Initialize()
+{
+    Super::Initialize();
+
+    return true;
+}
+
+void UTDResourceCard::NativePreConstruct()
+{
+    Super::NativePreConstruct();
+
+    TDSetImage(resourceTex);
+}
+
+void UTDResourceCard::NativeConstruct()
+{
+    Super::NativeConstruct();
+
+}
+
+void UTDResourceCard::TDSetImage(UTexture2D* _tex)
+{
+    if (IsValid(_tex))
+    {
+        resourceTex = _tex;
+        resourceImage->SetBrushFromTexture(resourceTex);
+    }
+}
--- Source/TowerDefense/UI/PauseMenu/TDResourceCard.h
+++ Source/TowerDefense/UI/PauseMenu/TDResourceCard.h
@@ -4,9 +4,37 @@
 #include "UI/TDUserWidget.h"
 #include "TDResourceCard.generated.h"
 
+class UImage;
+class UTexture2D;
+
 UCLASS()
 class TOWERDEFENSE_API UTDResourceCard : public UTDUserWidget
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+public:
+    virtual bool Initialize() override;
+    virtual void NativePreConstruct() override;
+    virtual void NativeConstruct() override;
+
+public:
+    UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+        UImage* resourceImage;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        UTexture2D* resourceTex;
+
+protected:
+
+
+private:
+
+
+public:
+    UFUNCTION()
+        void TDSetImage(UTexture2D* _tex);
+
+protected:
+
+
+private:
 };
